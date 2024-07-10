05c42877bf8e9a5aa7b573eab97960103ea46e74
JakkiFx <jackcielfelix555@gmail.com>
2024-07-09 23:55:45 -0300

    Teste de componente finalizado

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -27,10 +27,10 @@ describe("CoursesCardListComponent", () => {
   it("should display the course list", () => {
     const fixture = TestBed.createComponent(CoursesCardListComponent);
     const component = fixture.componentInstance;
-    const compiled = fixture.debugElement;
+    const compiled = fixture.debugElement; // Para acessar o DOM e depurar
 
     component.courses = setupCourses();
-    fixture.detectChanges(); // Detecta mudaças no DOM
+    fixture.detectChanges(); // Detecta mudanças no DOM
 
     const cards = compiled.queryAll(By.css(".course-card"));
 
@@ -39,6 +39,23 @@ describe("CoursesCardListComponent", () => {
   });
 
   it("should display the first course", () => {
-    pending();
+    const fixture = TestBed.createComponent(CoursesCardListComponent);
+    const component = fixture.componentInstance;
+    const compiled = fixture.debugElement;
+
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const course = component.courses[0]; 
+
+    // Selecionando um card especifico, seu titulo e imagem
+    const card = compiled.query(By.css(".course-card"));
+    const title = card.query(By.css("mat-card-title"));
+    const image = card.query(By.css("img"));
+
+    expect(card).toBeTruthy("Could not find course card");
+
+    // Verificando se o titulo e imagem correspondem
+    expect(title.nativeElement.textContent).toBe(course.titles.description);
+    expect(image.nativeElement.src).toBe(course.iconUrl);
   });
 });
